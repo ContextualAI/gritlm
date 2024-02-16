64b1d4c72789ffd0605bf7343f61c5cff7416ace
Muennighoff <n.muennighoff@gmail.com>
2024-02-16 01:26:13 +0100

    Reorder; 0.9.3

--- gritlm/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = "0.9.2"
-
-from gritlm import GritLM
--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ with open("README.md", mode="r", encoding="utf-8") as readme_file:
 
 setup(
     name='gritlm',
-    version='0.9.2',
+    version='0.9.3',
     description='GritLM',
     long_description=readme,
     long_description_content_type="text/markdown",
